0995b4dbaa4580396976a6ded566b77e133e380b
marfan <marfffan@gmail.com>
2025-01-05 17:22:19 +0300

    Оправка на проверку 05.01.25

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -32,6 +32,7 @@ public class UserDaoImp implements UserDao {
         String hql = "from User user " +
                 "join fetch user.car c " +
                 "where c.model = :model and c.series = :series";
+        //String hql = "from User user where user.car.model = :model and user.car.series = :series";
         TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
         query.setParameter("model", model).setParameter("series", series);
         return query.setMaxResults(1).getSingleResult();
